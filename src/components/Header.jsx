e129113e988570a171d79114b46fa33690d7a36c
maxbbkCmd <maxbbk.mtk@gmail.com>
2024-12-04 15:01:57 +0300

    Added components

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header className='header'>
+      <img src='https://snob.ru/indoc/tilda/995317/images/tild3130-3634-4366-b864-386464356430__acer.png' />
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Profile = () => {
+  return (
+    <div className='content'>
+      <div>
+        <img src='https://pbs.twimg.com/profile_banners/284131809/1591884155/1500x500' />
+      </div>
+      <div>ava + desription</div>
+      <div>
+        My posts
+        <div>New post</div>
+        <div>post 1</div>
+        <div>post 2</div>
+      </div>
+    </div>
+  );
+};
+
+export default Profile;
